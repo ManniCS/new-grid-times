ad02f6ad71115345e0906c329791ae360ebb0604
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-07-24 12:31:26 -0400

    Exercise 1 - First pass

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header>
       <SuperHeader>
-        <Row>
+        <MobileActions>
           <ActionGroup>
             <button>
               <Search size={24} />
@@ -26,10 +26,28 @@ const Header = () => {
               <User size={24} />
             </button>
           </ActionGroup>
-        </Row>
+        </MobileActions>
       </SuperHeader>
       <MainHeader>
+        <DesktopNavigationActions>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopNavigationActions>
         <Logo />
+        <DesktopSubscribeActions>
+          <SubscribeButton>
+            Subscribe
+          </SubscribeButton>
+          <LoginPrompt>
+            Already a subscriber?
+          </LoginPrompt> 
+        </DesktopSubscribeActions>
       </MainHeader>
     </header>
   );
@@ -39,9 +57,13 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media (${QUERIES.laptopAndUp}) { 
+    display: none;
+  }
 `;
 
-const Row = styled(MaxWidthWrapper)`
+const MobileActions = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
 `;
@@ -59,12 +81,53 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopNavigationActions = styled.div`
+  display: none;
+
+  @media (${QUERIES.laptopAndUp}) { 
+    display: revert;
+    color: var(--color-gray-900);
+  }
+`
+
+const SubscribeButton = styled(Button)`
+  margin-top: 22px;
+  margin-bottom: 2px;
+`
+const LoginPrompt = styled.a`
+  font-style: italic;  
+  font-family: var(--font-family-serif);
+  text-decoration: underline;
+  font-weight: 400;
+  color: var(--color-gray-900);
+`
+
+const DesktopSubscribeActions = styled.div`
+  display: none;
+
+  @media (${QUERIES.laptopAndUp}) { 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media (${QUERIES.laptopAndUp}) { 
+    justify-content: space-between;
+    margin-top: 22px;
+    margin-bottom: clamp(
+      3rem,
+      2.1vw + 1.5rem,
+      4rem
+    );
+  }
 `;
 
 export default Header;
